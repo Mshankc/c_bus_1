c330ea3c248cb9548fce2742f91607be7ab14e92
Data_paradox <74847598+mohammedek@users.noreply.github.com>
2023-12-04 21:02:23 +0530

    Merge PR 1

---
+++ lib/screens/item_info.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ItemInfo extends StatelessWidget {
+  const ItemInfo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("name"),
+      ),
+    );
+  }
+}
